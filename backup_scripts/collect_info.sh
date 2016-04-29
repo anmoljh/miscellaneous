06267bc67ba6ad24e26b55da2a93acb82b125bbe
anmoljh <anmol.jh@gmail.com>
2016-04-29 14:19:25 +0100

    updated 'collect_info.sh' file

--- backup_scripts/collect_info.sh
+++ backup_scripts/collect_info.sh
@@ -1,9 +1,20 @@
 
+
 mkdir -p collect_info
 
-pvdisplay >collect_info/pvdisplay.cmd
-vgdisplay >collect_info/vgdisplay.cmd
-lvdisplay >collect_info/lvdisplay.cmd
-lsblk     >collect_info/lsblk.cmd
-df -h     >collect_info/df-h.cmd
+# partition details
 
+pvdisplay >collect_info/pvdisplay-`date +%F`.cmd
+vgdisplay >collect_info/vgdisplay-`date +%F`.cmd
+lvdisplay >collect_info/lvdisplay-`date +%F`.cmd
+lsblk     >collect_info/lsblk-`date +%F`.cmd
+df -h     >collect_info/dfh-`date +%F`.cmd
+
+# network details
+
+ifconfig >collect_info/ifconfig-`date +%F`.cmd
+
+
+# firewall details
+
+iptables-save >collect_info/iptables-save-`date +%F`.cmd
